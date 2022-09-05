85170277fd081fbedc797a3d213d0899d4d941fe
Joseph Sutera <joesutera11@gmail.com>
2022-09-04 21:55:02 -0400

    commit tree 048f35ee73278b80186c21ccc602b91c11c36304

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+document.addEventListener('DOMContentLoaded',()=>{
+    fetch(imgUrl)
+    .then((resp)=>resp.json())
+    .then((data)=>data.message.forEach((element)=>{
+       const imgTag=document.createElement("img")
+       imgTag.src=element
+       const darkness= document.getElementById("dog-image-container")
+       darkness.append(imgTag)
+    }))
+    const ul = document.getElementById("dog-breeds")
+    let whoLetTheDogsIn= []
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+    .then((request)=>request.json())
+    .then((data)=>{
+         
+        whoLetTheDogsIn= Object.keys(data.message)
+        renderAllDogBreeds(whoLetTheDogsIn)
+       
+        })
+       function renderAllDogBreeds(dogs){
+        console.log('dogs',dogs)
+        ul.innerHTML=""
+        for(const dog of dogs){
+            const li=document.createElement("li")
+             li.textContent= dog 
+             ul.append(li)      
+        }
+       }
+
+        const options=document.getElementById("breed-dropdown")
+             options.addEventListener('change',(event)=>{
+        dogsOut(event.target.value)
+
+    function dogsOut(firstLetter){
+        const arrayOfDogs = whoLetTheDogsIn.filter((dogName)=>dogName[0]===firstLetter)
+      renderAllDogBreeds(arrayOfDogs)
+
+    }
+    
+})})
